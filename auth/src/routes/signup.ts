292220ee02ab8486ecf81dbce3b3b87d8c556cfd
Huy Tran <huytrn.dev@gmail.com>
2025-02-19 16:54:51 +0700

    chap 141

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
+import { RequestValidationError } from "../errors/request-validation-errors";
+import { DatabaseConnectionError } from "../errors/database-connection-errors";
 
 const router = express.Router();
 
@@ -16,10 +18,10 @@ router.post(
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-       throw new Error('Invalid email or password');
+       throw new RequestValidationError(errors.array());
     }
     console.log('Creating a new user... ')
-    throw new Error('Failed to connect to database');
+    throw new DatabaseConnectionError();
     res.send({});
   }
 );
