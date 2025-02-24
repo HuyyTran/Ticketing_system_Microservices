12cfdee5cb2f068e78affdbea741f3e20e4283d5
HuyyTran <huytrn.dev@gmail.com>
2025-02-24 18:08:43 +0700

    chap 210

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -4,16 +4,25 @@ import { app } from "../../app";
 // supertest by default does not manage cookies, so we need to manually manage them
 
 it("responds with details about the current user", async () => {
-  await request(app)
-    .post("/api/users/signup")
-    .send({
-      email: "test@test.com",
-      password: "password",
-    })
-    .expect(201);
+	const authResponse = await request(app)
+		.post("/api/users/signup")
+		.send({
+			email: "test@test.com",
+			password: "password",
+		})
+		.expect(201);
 
-  const response = await request(app)
-    .get("/api/users/currentuser")
-    .send({})
-    .expect(200);
+	const cookie = authResponse.get("Set-Cookie");
+
+	if (!cookie) {
+		throw new Error("Cookie not set after signup");
+	}
+
+	const response = await request(app)
+		.get("/api/users/currentuser")
+		.set("Cookie", cookie)
+		.send({})
+		.expect(200);
+    
+    expect(response.body.currentUser.email).toEqual("test@test.com");
 });
