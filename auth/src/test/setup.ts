9c9123a8d5ea612bc9e29dbc050413d095714f68
HuyyTran <huytrn.dev@gmail.com>
2025-02-24 17:27:22 +0700

    chap 202

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -5,6 +5,7 @@ import { app } from "../app";
 let mongo: any;
 
 beforeAll(async () => {
+  process.env.JWT_KEY = "asdfdsa";
   mongo = await MongoMemoryServer.create();
   const mongoUri = mongo.getUri();
   await mongoose.connect(mongoUri, {});
