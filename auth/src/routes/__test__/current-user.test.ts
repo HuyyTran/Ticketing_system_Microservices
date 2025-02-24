46442e6c68f6d878987a23079b892baadab057ad
HuyyTran <huytrn.dev@gmail.com>
2025-02-24 18:24:17 +0700

    chap 213

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -4,19 +4,7 @@ import { app } from "../../app";
 // supertest by default does not manage cookies, so we need to manually manage them
 
 it("responds with details about the current user", async () => {
-	const authResponse = await request(app)
-		.post("/api/users/signup")
-		.send({
-			email: "test@test.com",
-			password: "password",
-		})
-		.expect(201);
-
-	const cookie = authResponse.get("Set-Cookie");
-
-	if (!cookie) {
-		throw new Error("Cookie not set after signup");
-	}
+    const cookie = await global.signin();
 
 	const response = await request(app)
 		.get("/api/users/currentuser")
--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,9 +1,13 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
+import request from "supertest";
 import { app } from "../app";
 
-let mongo: any;
+declare global {
+	var signin: () => Promise<string[]>;
+}
 
+let mongo: any;
 beforeAll(async () => {
 	process.env.JWT_KEY = "asdfdsa";
 	mongo = await MongoMemoryServer.create();
@@ -27,3 +31,24 @@ afterAll(async () => {
 	}
 	await mongoose.connection.close();
 });
+
+global.signin = async () => {
+	const email = "test@test.com";
+	const password = "password";
+
+	const response = await request(app)
+		.post("/api/users/signup")
+		.send({
+			email,
+			password,
+		})
+		.expect(201);
+
+	const cookie = response.get("Set-Cookie");
+
+	if (!cookie) {
+		throw new Error("Failed to get cookie from response");
+	}
+
+	return cookie;
+};
