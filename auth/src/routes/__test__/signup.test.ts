6cdbd914758ce11fcff74abaf277958de83495d4
HuyyTran <huytrn.dev@gmail.com>
2025-02-24 17:36:36 +0700

    chap 203

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -1,12 +1,49 @@
 import request from "supertest";
 import { app } from "../../app";
+import e from "express";
 
 it("returns a 201 on successful signup", async () => {
-  return request(app)
+  await request(app)
     .post("/api/users/signup")
     .send({
       email: "test@test.com",
       password: "password",
     })
     .expect(201);
+});
+
+it("returns a 400 with an invalid email", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test.com",
+      password: "password",
+    })
+    .expect(400);
+});
+
+it("returns a 400 with an invalid password", async () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@tesst.com",
+      password: "p",
+    })
+    .expect(400);
+});
+
+it("returns a 400 with missing email and password", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      password: "123213",
+    })
+    .expect(400);
+    
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "tesst@tets.com",
+    })
+    .expect(400);
 });
