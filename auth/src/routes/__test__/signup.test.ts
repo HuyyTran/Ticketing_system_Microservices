65801f72d4dcaa1bc18dada22f4ec0ca00db923c
HuyyTran <huytrn.dev@gmail.com>
2025-02-24 17:38:26 +0700

    chap 204

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -39,11 +39,29 @@ it("returns a 400 with missing email and password", async () => {
       password: "123213",
     })
     .expect(400);
-    
+
   await request(app)
     .post("/api/users/signup")
     .send({
       email: "tesst@tets.com",
+    })
+    .expect(400);
+});
+
+it("disallows duplicate emails", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(201);
+
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "password",
     })
     .expect(400);
 });
