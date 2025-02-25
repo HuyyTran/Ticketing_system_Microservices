2403875b8c26c674de59e44dbb8da1a6448fa253
HuyyTran <huytrn.dev@gmail.com>
2025-02-25 23:02:07 +0700

    error: chap 263

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -2,12 +2,14 @@ import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
 import cookieSession from "cookie-session";
+import { errorHandler, NotFoundError } from "@datn242/common";
+// import { errorHandler } from "./middlewares/error-handler";
+// import { NotFoundError } from "./errors/not-found-error";
 
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signupRouter } from "./routes/signup";
 import { signoutRouter } from "./routes/signout";
-import { errorHandler, NotFoundError } from "@datn242/common";
 
 const app = express();
 app.set("trust proxy", true);
