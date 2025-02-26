0de00c85e6dbb04b77add6b6acd7e6cbb2c918b8
Huy Tran <huytrn.dev@gmail.com>
2025-02-26 16:17:33 +0700

    chap 273

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,7 +2,7 @@ import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
 import cookieSession from "cookie-session";
-import { errorHandler, NotFoundError } from "@datn242/common";
+import { errorHandler, NotFoundError, currentUser } from "@datn242/common";
 import { createTicketRouter } from "./routes/new";
 
 const app = express();
@@ -14,6 +14,8 @@ app.use(
 		secure: process.env.NODE_ENV !== "test", // only use cookies over https, unless in test environment
 	}),
 );
+
+app.use(currentUser);
 
 app.use(createTicketRouter);
 
