7e643bcc20ad5c5832a1a78b50541b8040a820f5
Huy Tran <huytrn.dev@gmail.com>
2025-02-26 16:06:23 +0700

    chap 272

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -3,6 +3,7 @@ import "express-async-errors";
 import { json } from "body-parser";
 import cookieSession from "cookie-session";
 import { errorHandler, NotFoundError } from "@datn242/common";
+import { createTicketRouter } from "./routes/new";
 
 const app = express();
 app.set("trust proxy", true);
@@ -13,6 +14,8 @@ app.use(
 		secure: process.env.NODE_ENV !== "test", // only use cookies over https, unless in test environment
 	}),
 );
+
+app.use(createTicketRouter);
 
 app.all("*", async (req, res) => {
 	throw new NotFoundError();
