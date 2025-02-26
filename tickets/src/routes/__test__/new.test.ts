056c552933a7056119d8b70f728a0d64f4d30705
Huy Tran <huytrn.dev@gmail.com>
2025-02-26 15:56:16 +0700

    chap 271

---
+++ tickets/src/routes/__test__/new.test.ts
@@ -0,0 +1,12 @@
+import request from "supertest";
+import { app } from "../../app";
+
+it("has a route handler listening to /api/tickets for post requests", async () => {});
+
+it("can only be accessed if user is signed in", async () => {});
+
+it("return an error if an invalid title is provided", async () => {});
+
+it("return an error if an invalid price is provided", async () => {});
+
+it("create a ticket with valid input", async () => {});
