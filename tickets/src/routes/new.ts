682780ce9b543a1653aebc9e42b7b1e48e77581e
Huy Tran <huytrn.dev@gmail.com>
2025-03-07 16:17:23 +0700

    chap 339

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -3,6 +3,7 @@ import { body } from 'express-validator';
 import { requireAuth, validateRequest } from '@datn242/common';
 import { Ticket } from '../models/ticket';
 import { TicketCreatedPublisher } from '../events/publishers/ticket-created-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = require('express').Router();
 
@@ -22,7 +23,7 @@ router.post(
 
     await ticket.save();
 
-    new TicketCreatedPublisher(client).publish({
+    new TicketCreatedPublisher(natsWrapper.client).publish({
       id: ticket.id,
       title: ticket.title,
       price: ticket.price,
