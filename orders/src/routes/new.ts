dd727e81255ead1cf3b222d5a491d6fffa6d2ae7
HuyyTran <huytrn.dev@gmail.com>
2025-03-10 13:56:51 +0700

    chap 356

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,24 @@
+import mongoose from 'mongoose'; //optional
 import express, { Request, Response } from 'express';
+import { requireAuth, validateRequest } from '@datn242/common';
+import { body } from 'express-validator';
 
 const router = express.Router();
 
-router.post('/api/orders', async (req: Request, res: Response) => {
-  res.send({});
-});
+router.post(
+  '/api/orders',
+  requireAuth,
+  [
+    body('ticketId')
+      .not()
+      .isEmpty()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input)) //optional mongoID check
+      .withMessage('TicketId is required'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    res.send({});
+  },
+);
 
 export { router as newOrderRouter };
