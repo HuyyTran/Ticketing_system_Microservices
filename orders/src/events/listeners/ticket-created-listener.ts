a3b8a6fbddde08b7dd000d1142dc30d3c6e0e641
HuyyTran <huytrn.dev@gmail.com>
2025-03-11 17:53:43 +0700

    chap 389

--- orders/src/events/listeners/ticket-created-listener.ts
+++ orders/src/events/listeners/ticket-created-listener.ts
@@ -1,10 +1,11 @@
 import { Message } from 'node-nats-streaming';
 import { Listener, TicketCreatedEvent, Subjects } from '@datn242/common';
 import { Ticket } from '../../models/ticket';
+import { queueGroupName } from './queue-group-name';
 
 export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
   subject: Subjects.TicketCreated = Subjects.TicketCreated;
-  queueGroupName = 'orders-service';
+  queueGroupName = queueGroupName;
 
   async onMessage(data: TicketCreatedEvent['data'], msg: Message) {}
 }
