ee3c9f5b7ec8c7364e44aeeb49547b79b1ba7186
HuyyTran <huytrn.dev@gmail.com>
2025-03-11 11:32:15 +0700

    chap 363

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,7 +1,14 @@
 import mongoose from 'mongoose'; //optional
 import express, { Request, Response } from 'express';
-import { requireAuth, validateRequest } from '@datn242/common';
+import {
+  NotFoundError,
+  requireAuth,
+  validateRequest,
+  NotFoundError,
+} from '@datn242/common';
 import { body } from 'express-validator';
+import { Ticket } from '../models/ticket';
+import { Order } from '../models/order';
 
 const router = express.Router();
 
@@ -17,6 +24,20 @@ router.post(
   ],
   validateRequest,
   async (req: Request, res: Response) => {
+    const { ticketId } = req.body;
+    // Find the ticket the user is trying to order in the database
+    const ticket = await Ticket.findById(ticketId);
+    if (!ticket) {
+      throw new NotFoundError('Ticket not found');
+    }
+
+    // Make sure that this ticket is not already reserved
+
+    // Calculate an expiration date for this order
+
+    // Build the order and save it to the database
+
+    // Publish an event saying that an order was created
     res.send({});
   },
 );
