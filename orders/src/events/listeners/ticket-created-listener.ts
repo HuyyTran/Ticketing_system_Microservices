0eca99347853435570fc203e113caef73aa7fe02
HuyyTran <huytrn.dev@gmail.com>
2025-03-11 17:56:43 +0700

    chap 390

--- orders/src/events/listeners/ticket-created-listener.ts
+++ orders/src/events/listeners/ticket-created-listener.ts
@@ -7,5 +7,13 @@ export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
   subject: Subjects.TicketCreated = Subjects.TicketCreated;
   queueGroupName = queueGroupName;
 
-  async onMessage(data: TicketCreatedEvent['data'], msg: Message) {}
+  async onMessage(data: TicketCreatedEvent['data'], msg: Message) {
+    const { title, price } = data;
+    const ticket = Ticket.build({
+      title,
+      price,
+    });
+    await ticket.save();
+    msg.ack();
+  }
 }
