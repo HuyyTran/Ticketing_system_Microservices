bd7b64eb9a62cebf59d0d00607ab04dbec4fb209
HuyyTran <huytrn.dev@gmail.com>
2025-03-11 15:37:16 +0700

    chap 378

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,9 +1,28 @@
 import express, { Request, Response } from 'express';
+import {
+  requireAuth,
+  NotAuthorizedError,
+  NotFoundError,
+} from '@datn242/common';
+import { Order } from '../models/order';
+import { OrderStatus } from '@datn242/common';
 
 const router = express.Router();
 
 router.delete('/api/orders/:orderID', async (req: Request, res: Response) => {
-  res.send({});
+  //actually, this is a patch request
+  const { orderID } = req.params;
+  const order = await Order.findById(orderID);
+
+  if (!order) {
+    throw new NotFoundError();
+  }
+  if (order.userId !== req.currentUser!.id) {
+    throw new NotAuthorizedError();
+  }
+  order.status = OrderStatus.Cancelled;
+  await order.save();
+  res.status(204).send(order);
 });
 
 export { router as deleteOrderRouter };
