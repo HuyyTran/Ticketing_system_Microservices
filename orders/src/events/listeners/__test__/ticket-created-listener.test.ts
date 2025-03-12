f2cb7ffd18fc2b6d8637c6e9121f9cc0c1b8f9cb
Huy Tran <huytrn.dev@gmail.com>
2025-03-12 17:33:25 +0700

    chap 411

---
+++ orders/src/events/listeners/__test__/ticket-created-listener.test.ts
@@ -0,0 +1,14 @@
+const setup = async () => {
+  // create an instance of a listener
+  // create a fake data event
+  // create a fake message object
+};
+it('creates and saves a ticket', async () => {
+  // call the onMessage function with the data object + message object
+  // write assertions to make sure a ticket was created
+});
+
+it('acks a message', async () => {
+  // call the onMessage function with the data object + message object
+  // write assertions to make sure the ack function was called
+});
